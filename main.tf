1ff0dcdb79f1ff593f7185cb208add5ceb516b50
M Saeed Shaikh <g555066@genmills.com>
2020-05-26 09:03:30 -0500

    test

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Configure the Alicloud Provider
+
+
+resource "alicloud_vpc" "vpc" {
+#  count = "${var.env == "sandbox" ? 1 : 0}"
+  name = "mumbai_vpc"
+  cidr_block = var.vpc_cidr
+}
+
+
+# Loop and if condition example
+resource alicloud_vswitch "vswitch_1" {
+  count = length(var.zones)
+  vpc_id = alicloud_vpc.vpc.id
+  cidr_block = element(var.vswitch_cidr,count.index)
+  availability_zone = element(var.zones,count.index)
+  name = "vswitch1-${count.index}"
+}
+
+
