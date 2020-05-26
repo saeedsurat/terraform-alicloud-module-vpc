383d249f6bd40c6182c4f98c29c3d0058f4b15fd
M Saeed Shaikh <g555066@genmills.com>
2020-05-26 10:15:13 -0500

    test

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "alicloud_vpc" "vpc" {
 #  count = "${var.env == "sandbox" ? 1 : 0}"
   name = "mumbai_vpc"
   cidr_block = var.vpc_cidr
+  tag = "vpc_mumbai"
 }
 
 
@@ -15,6 +16,7 @@ resource alicloud_vswitch "vswitch_1" {
   cidr_block = element(var.vswitch_cidr,count.index)
   availability_zone = element(var.zones,count.index)
   name = "vswitch1-${count.index}"
+  tag = ${count.index}
 }
 
 
