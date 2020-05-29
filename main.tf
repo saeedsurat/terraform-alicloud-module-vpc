0d01344caef4ccf5d8e704cdab00cda0e85f16a2
M Saeed Shaikh <g555066@genmills.com>
2020-05-29 04:24:02 -0500

    test

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "alicloud_vpc" "vpc" {
 #  count = "${var.env == "sandbox" ? 1 : 0}"
   name = "mumbai_vpc"
   cidr_block = var.vpc_cidr
-  tags = "vpc_mumbai"
+#  tags = "vpc_mumbai"
 }
 
 # Loop and if condition example
@@ -17,10 +17,10 @@ resource alicloud_vswitch "vswitch_1" {
   name = "vswitch1-${count.index}"
 #  tags = "vswitch_1"
 
-  tags = {
-    Created      = "Terraform"
-    Environment = "dev"
-  }
+#  tags = {
+#    Created      = "Terraform"
+#    Environment = "dev"
+#  }
 }
 
 
